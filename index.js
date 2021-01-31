551227ade912011db5f759b913d42f2496efebcf
박진수 <parkjinsu@bagjinsuui-MacBookPro.local>
2021-01-31 15:16:36 +0900

    post update delete

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const { buildSchema } = require('graphql');
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
 
+    input ProductInput {
+        name : String
+        price : Int
+        description : String
+    }
+
     type Product{
         id: ID!
         name: String
@@ -13,8 +19,15 @@ const schema = buildSchema(`
     }
 
     type Query {
-        getProduct(id : ID!): Product
+        getProduct(id : ID!): Product ,
     }
+
+    type Mutation {
+        addProduct( input : ProductInput ) : Product
+        updateProduct(id: ID! , input: ProductInput!) : Product
+        deleteProduct(id:ID!): String
+    }
+
 `);
 
 //임시데이터
@@ -34,12 +47,36 @@ const products = [
 ]
 //임시데이터
 
-
-
 const root = {
     getProduct: ({ id }) => products.
         find(product =>
-            product.id === parseInt(id))
+            product.id === parseInt(id)),
+
+    addProduct: ({ input }) => {
+        //임시변수에 id값을 +1 해서 데이터를 추가하겠다.
+        input.id = parseInt(products.length + 1);
+        //입력받은 데이터를 추가하겠다.
+        products.push(input);
+        //재활용 
+        return root.getProduct({ id: input.id });
+    },
+
+
+    //인덱싱값을 알아야한다
+    updateProduct: ({ id, input }) => {
+        const index = products.findIndex(product => product.id === parseInt(id))
+        products[index] = {
+            id: parseInt(id),
+            ...input
+        }
+        return products[index];
+    },
+
+    deleteProduct: ({ id }) => {
+        const index = products.findIndex(product => product.id === parseInt(id))
+        products.splice(index, 1)
+        return '성공'
+    }
 };
 
 const app = express();
@@ -51,4 +88,18 @@ app.use('/graphql', graphqlHTTP({
 
 app.listen(4000, () => {
     console.log('Running a GraphQL API server at localhost:4000/graphql');
-});  
+});
+
+
+//추가
+// {
+//     "query": "mutation updateProduct( $id : ID! , $input: ProductInput! ) { updateProduct( id : $id  , input: $input) { id } }",
+//     "variables": { "id" : 1 ,"input" : { "name" : "수정상품" , "price" : 1000 , "description" : "후후후" } }
+// }
+
+
+//수정
+// {
+//     "query": "mutation updateProduct( $id : ID! , $input: ProductInput! ) { updateProduct( id : $id  , input: $input) { id } }",
+//     "variables": { "id" : 1 ,"input" : { "name" : "수정상품" , "price" : 1000 , "description" : "후후후" } }
+// }
